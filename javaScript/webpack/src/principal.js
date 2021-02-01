deb6eeedbe722240b292a31e2b2e9396b931c7d1
Brunno <brzonatto@gmail.com>
2021-02-01 17:37:33 -0300

    WEBPACK - continuação

--- javaScript/webpack/src/principal.js
+++ javaScript/webpack/src/principal.js
@@ -1,5 +1,7 @@
 // const Pessoa = require('./pessoa.js')
 import Pessoa from './pessoa'
+import './modulos/moduloA'
+import './assets/css/estilo.css'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- javaScript/webpack/webpack.config.js
+++ javaScript/webpack/webpack.config.js
@@ -2,5 +2,18 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: 'development', // ou 'production'
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader' // interpreta @import, url()...
+            ]
+        }]
+    }
 }
