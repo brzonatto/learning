6c0cb39777282c73b6f8fb15927cd42fa4c864d0
Brunno <brzonatto@gmail.com>
2021-03-15 17:39:48 -0300

    React - Componente de Classe

--- javaScript/react/exercicios/src/index.js
+++ javaScript/react/exercicios/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 
 // import Primeiro from './componentes/Primeiro'
 // import BomDia from './componentes/BomDia'
-import { BoaTarde, BoaNoite } from './componentes/Multiplos'
+// import { BoaTarde, BoaNoite } from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
 // const elemento = <h1>React 2</h1>
 
 ReactDOM.render(
     <div>
-        <BoaTarde nome="Brunno"/>
-        <BoaNoite nome="Carol"/>
+        <Saudacao tipo="Bom dia" nome="Brunno"/>        
     </div>,
     document.getElementById('root')
 )
