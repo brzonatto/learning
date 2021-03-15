73b103bf429d0a4c8f6dd87a3b91fe6655bb30f2
Brunno <brzonatto@gmail.com>
2021-03-15 15:25:52 -0300

    React - Primeiro componente

--- javaScript/react/exercicios/src/index.js
+++ javaScript/react/exercicios/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './componentes/Primeiro'
+
 const elemento = <h1>React 2</h1>
 
-ReactDOM.render(elemento, document.getElementById('root'))
+ReactDOM.render(<Primeiro/>, document.getElementById('root'))
 
