250149e32bd59685d35718e5fada1e86fdbfc8aa
Brunno Zonatto <brzonatto@gmail.com>
2021-03-16 22:51:23 -0300

    React - Projeto Calculadora

--- javaScript/react/calculadora/src/components/Button.jsx
+++ javaScript/react/calculadora/src/components/Button.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './Button.css'
 
 export const Button = props => {
-    let classes = 'button'
+    let classes = 'button '
     classes += props.operation ? 'operation' : ''
     classes += props.double ? 'double' : ''
     classes += props.triple ? 'triple' : ''
 
     return (
-    <button onClick={props.clck && props.click(props.label)} className={classes}>
+    <button onClick={e => props.click && props.click(e.target.innerHTML)} className={classes}>
         {props.label}
     </button>
     )
